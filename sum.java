fe19949e8938d97fa0c709a707c6861ba67d6bc6
ravindra <ravindra.devops@gmail.com>
2020-12-03 10:41:40 +0530

    test message

---
+++ sum.java
@@ -0,0 +1,13 @@
+class sum
+{
+	public static void main(String[] args) 
+	{
+	    int number1=30, number2=60, number3=80;
+		
+		System.out.println(" THe number one is ");
+System.out.println(" THe number Two is "+number2);
+System.out.println(" THe number Three is "+number3);
+
+	}
+}
+
